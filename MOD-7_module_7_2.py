5f862e3b0478d4c2c38461a029e712cf30fd9025
Selemin Yuriy <71262890+yuriselemin@users.noreply.github.com>
2024-07-08 23:17:45 +0300

    Add files via upload

---
+++ MOD-7_module_7_2.py
@@ -0,0 +1,26 @@
+
+# Домашнее задание по теме "Позиционирование в файле".
+
+
+
+def custom_write(file_name, strings):
+    f = open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    for i, string in enumerate(strings):
+        f.write(string + '\n')
+        start_position = f.tell()
+        strings_positions[(i + 1, start_position)] = string
+    f.close()
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
